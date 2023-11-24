e02ac1d1ff4a493c67a6a795080ed51c78c52194
kento <kento121406@gmail.com>
2023-11-24 13:44:31 +0900

    発展課題 if文

--- #3/test_play.c
+++ #3/test_play.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 int main(){
-    if(0)
-        printf("牛丼温玉乗せ\n");
-    else if(0)
-        printf("豚丼\n");
-    else
-        printf("昼食抜き\n");
+    int a = 6;
+    if (a > 5){
+        printf("aは5より大きい\n");
+    }
+    return 0;
 }
