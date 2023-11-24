fcfc39e0c730ce58a483b0df69435fc98001f367
kento <kento121406@gmail.com>
2023-11-24 13:35:17 +0900

    結果:エラー

--- #3/test_play.c
+++ #3/test_play.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-int main(){
-    if(true)
+int test(){
+    if(0)
         printf("牛丼温玉乗せ\n");
-    else if(1)
+    else if(0)
         printf("豚丼\n");
     else
         printf("昼食抜き\n");
