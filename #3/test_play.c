54bdccdcc7bdddd961649870b02e411cd75872e1
kento <kento121406@gmail.com>
2023-11-24 13:27:33 +0900

    結果:牛丼温玉乗せ

--- #3/test_play.c
+++ #3/test_play.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-    if(0)
+    if(2)
         printf("牛丼温玉乗せ\n");
     else if(1)
         printf("豚丼\n");
     else
         printf("昼食抜き\n");
-        return 0;
+    return 0;
 }
