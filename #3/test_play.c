5fc1157c62d1a6a050919036ef5bdc8fe71f7918
kento <kento121406@gmail.com>
2023-11-24 13:26:20 +0900

    牛丼0、豚丼1

--- #3/test_play.c
+++ #3/test_play.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main(){
-
-    if(1)
-        printf("Hello!\n");
+    if(0)
+        printf("牛丼温玉乗せ\n");
+    else if(1)
+        printf("豚丼\n");
+    else
+        printf("昼食抜き\n");
+        return 0;
 }
